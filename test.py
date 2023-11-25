b40833fe84f553cc8626930b16052d37e304e0db
ColasssNotMe <81697922+ColasssNotMe@users.noreply.github.com>
2023-11-25 22:28:47 +0800

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import json
-
-data = {"num1": "10", "num2": "20", "num3": "30"}
-
-with open("data.json", "w") as f:
-    json.dump(data, f)
-
-with open("data.json", "r") as f:
-    print(json.load(f["num1"]))
